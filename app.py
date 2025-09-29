c6aa21ec0f14fea075cbf9af3a056201eae88a0f
mrjoj509 <77214665+mrjoj509@users.noreply.github.com>
2025-09-29 20:34:17 +0300

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ except ImportError:
 # ============================================
 class Network:
     def __init__(self):
-        self.proxy = "http://infproxy_checkemail509:NLI8oq4ZQC2fJ3yJDcSv@proxy.infiniteproxies.com:1111"
+        self.proxy = "infproxy_checkemail509:NLI8oq4ZQC2fJ3yJDcSv@proxy.infiniteproxies.com:1111"
 
         self.hosts = [
             "api31-normal-useast2a.tiktokv.com",
